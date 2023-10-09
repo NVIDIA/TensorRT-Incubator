4e61701308b7725114c8a8655695aa082a07a3b7
Parth Chadha <pchadha@nvidia.com>
2023-10-09 16:33:37 -0700

    #2 Improve NamedDim API to provide runtime shape separately from optimization profile.

--- tests/frontend/test_named_dim.py
+++ tests/frontend/test_named_dim.py
@@ -3,23 +3,34 @@ import pytest
 
 
 class TestNamedDim:
-    def test_named_dim_repr(self):
-        dim = NamedDim("dim", (2, 3, 4))
-        assert repr(dim) == 'NamedDim(name="dim", dim_range=(2, 3, 4))'
+    @pytest.mark.parametrize("range", [2, 3])
+    def test_named_dim_repr(self, range):
+        dim = NamedDim("dim", 2, max=range)
+        assert repr(dim) == f'NamedDim(name="dim", runtime_value=2, min=2, opt=2, max={range})'
         eval_dim = eval(repr(dim))
-        assert eval_dim.min == 2 and eval_dim.opt == 3 and eval_dim.max == 4
+        assert eval_dim.min == 2 and eval_dim.opt == 2 and eval_dim.max == range and eval_dim.runtime_value == 2
 
-    @pytest.mark.parametrize("shape", [2, (2, 3), (2, 3, 4)])
-    def test_named_dim_profile(self, shape):
-        dim = NamedDim("dim", shape)
-        assert dim.min == shape if isinstance(shape, int) else shape[0]
-        assert dim.opt == shape if isinstance(shape, int) else shape[1]
-        assert dim.max == shape if isinstance(shape, int) else shape[-1]
+    @pytest.mark.parametrize("dim_range", [{"min": 3, "max": 5}, {"min": 3, "max": 5, "opt": 4}])
+    def test_named_dim_profile_dict(self, dim_range):
+        dim = NamedDim("dim", runtime_value=3, **dim_range)
+        assert dim.min == dim_range["min"]
+        assert dim.max == dim_range["max"]
+        assert dim.opt == dim_range.get("opt", int((dim_range["max"] + dim_range["min"]) / 2.0))
 
-    @pytest.mark.parametrize("shape", [{2}, (2, 3, 5, 6), 3.4])
-    def test_named_dim_rejects_invalid_shape(self, shape):
-        with pytest.raises(ValueError):
-            dim = NamedDim("dim", shape)
+    @pytest.mark.parametrize("runtime_value", [2])
+    @pytest.mark.parametrize("dim_range", [(3)])
+    def test_named_dim_profile_out_of_range(self, runtime_value, dim_range):
+        with pytest.raises(AssertionError):
+            dim = NamedDim("dim", runtime_value, min=3)
+
+    def test_named_dim_validate_set_runtime_value(self):
+        dim = NamedDim("dim", runtime_value=3, min=2, opt=4, max=6)
+        dim.runtime_value = 5
+
+    def test_named_dim_validate_rejects_invalid_runtime_value(self):
+        with pytest.raises(AssertionError):
+            dim = NamedDim("dim", runtime_value=3, min=2, opt=4, max=6)
+            dim.runtime_value = 1
 
     # Verify that read only property can not be set
     def test_named_dim_rejects_setting_dim_range(self):
@@ -28,3 +39,7 @@ class TestNamedDim:
             dim.min = 4
             dim.opt = 4
             dim.max = 4
+
+    def test_incomplete_profile(self):
+        with pytest.raises(AssertionError):
+            dim = NamedDim("d", 2, opt=3)
---
+++ tripy/util/util.py
@@ -0,0 +1,12 @@
+def default(value, default):
+    """
+    Returns a specified default value if the provided value is None.
+
+    Args:
+        value : The value.
+        default : The default value to use if value is None.
+
+    Returns:
+        object: Either value, or the default.
+    """
+    return value if value is not None else default
