7ca8fe60ff0776d942b71aacc7f327d2459632e4
Yizhuo Zhang <yizhuoz@nvidia.com>
2023-11-15 15:50:27 -0800

    #22: Upload artifacts to Gitlab packages & enable cache in CI

--- scripts/download_dependencies.py
+++ scripts/download_dependencies.py
@@ -8,14 +8,13 @@ def main():
         print("Found stablehlo package, skipping download.")
     else:
         print("Downloading stablehlo package from CI...")
-        # TODO: replace the link with the latest artifacts on main branch
         subprocess.run(
             [
-                'curl --location --output artifacts.zip --location --header "PRIVATE-TOKEN: $TRIPY_GITLAB_API_TOKEN" "https://gitlab-master.nvidia.com/api/v4/projects/104296/jobs/73969965/artifacts"'
+                'curl --header "PRIVATE-TOKEN: $TRIPY_GITLAB_API_TOKEN" "https://gitlab-master.nvidia.com/api/v4/projects/104296/packages/generic/dependencies/default/stablehlo.zip" --output stablehlo.zip'
             ],
             shell=True,
         )
-        subprocess.run(["unzip artifacts.zip && rm artifacts.zip"], shell=True)
+        subprocess.run(["unzip -q stablehlo.zip && rm stablehlo.zip"], shell=True)
     # download mlir-tensorrt
     if os.path.exists("mlir-tensorrt"):
         print("Found mlir-tensorrt package, skipping download.")
@@ -23,14 +22,13 @@ def main():
         diff_files = subprocess.run(["git diff origin/HEAD --name-only"], shell=True, capture_output=True, text=True)
         if "mlir-tensorrt.txt" not in diff_files.stdout.split("\n"):
             print("Downloading mlir-tensorrt package from CI...")
-            # TODO: replace the link with the latest artifacts on main branch
             subprocess.run(
                 [
-                    'curl --location --output artifacts.zip --location --header "PRIVATE-TOKEN: $TRIPY_GITLAB_API_TOKEN" "https://gitlab-master.nvidia.com/api/v4/projects/104296/jobs/73969966/artifacts"'
+                    'curl --header "PRIVATE-TOKEN: $TRIPY_GITLAB_API_TOKEN" "https://gitlab-master.nvidia.com/api/v4/projects/104296/packages/generic/dependencies/default/mlir-tensorrt.zip" --output mlir-tensorrt.zip'
                 ],
                 shell=True,
             )
-            subprocess.run(["unzip artifacts.zip && rm artifacts.zip"], shell=True)
+            subprocess.run(["unzip -q mlir-tensorrt.zip && rm mlir-tensorrt.zip"], shell=True)
         else:
             print("mlir-tensorrt version has been changed, please build the package locally first!")
 
