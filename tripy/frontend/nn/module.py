e7e42458464bedbdaad1b90c5662172c80b1fe07
Parth Chadha <pchadha@nvidia.com>
2023-12-18 10:27:09 -0800

    Move imports to top of the file; remove redundant statement

--- tripy/frontend/nn/module.py
+++ tripy/frontend/nn/module.py
@@ -15,11 +15,12 @@ class Module:
 
         class Network(Module):
             def __init__(self):
-                super().__init__()
                 from tripy.frontend.nn.parameter import Parameter
                 from tripy.frontend import Tensor
                 import numpy as np
-                self.param = Parameter(Tensor(np.ones([2])))
+
+                super().__init__()
+                self.param = Parameter(Tensor(np.ones(2, dtype=np.float32)))
 
             def __call__(self, x):
                 return x + self.param
@@ -28,10 +29,10 @@ class Module:
         def infer(net):
             from tripy.frontend import Tensor
             import numpy as np
-            x = Tensor(np.ones(2))
+            x = Tensor(np.ones(2, dtype=np.float32))
             return net(x)
 
-        assert(infer(net).eval() == np.array([2.0, 2.0])).all()
+        assert(infer(net).eval().cpu_view(np.float32) == np.array([2.0, 2.0])).all()
     """
 
     _params: Dict[str, Any]
