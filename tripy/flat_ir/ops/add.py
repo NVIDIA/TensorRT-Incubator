72884f8ed945debb2cc52a1ac9df785ddb98d528
Parth Chadha <pchadha@nvidia.com>
2024-01-08 11:47:33 -0800

    Fix flat_ir str representation to include shape, dtype, loc information

--- tripy/flat_ir/ops/add.py
+++ tripy/flat_ir/ops/add.py
@@ -4,6 +4,7 @@ from mlir import ir
 from mlir.dialects import stablehlo
 
 from tripy.flat_ir.ops.base import BaseFIROp
+from tripy.util.util import get_flat_tensor_info
 
 
 class AddOp(BaseFIROp):
@@ -16,7 +17,7 @@ class AddOp(BaseFIROp):
 
     def to_flat_ir_str(self, input_names, output_names) -> str:
         assert len(output_names) == 1, "AddOp should have exactly one output!"
-        return f"{output_names[0]} = {self.__class__.__name__} {' '.join(input_names)}"
+        return f"{output_names[0]} = {self.__class__.__name__} {', '.join([f'{get_flat_tensor_info(name, self.inputs[idx])}' for idx, name in enumerate(input_names)])}"
 
     def to_mlir(self, operands: List) -> List:
         add_out = stablehlo.AddOp(*operands)
--- tripy/flat_ir/ops/copy.py
+++ tripy/flat_ir/ops/copy.py
@@ -3,6 +3,7 @@ from typing import List
 from mlir import ir
 
 from tripy.flat_ir.ops.base import BaseFIROp
+from tripy.util.util import get_flat_tensor_info
 
 
 class CopyOp(BaseFIROp):
@@ -17,7 +18,7 @@ class CopyOp(BaseFIROp):
 
     def to_flat_ir_str(self, input_names, output_names) -> str:
         assert len(output_names) == 1, "CompareOp should have exactly one output!"
-        return f"{output_names[0]} : {self.__class__.__name__} copy={input_names[0]}, target={self.target.kind}:{self.target.index})"
+        return f"{output_names[0]} = {self.__class__.__name__} copy={get_flat_tensor_info(input_names[0], self.inputs[0])}, target={self.target.kind}:{self.target.index})"
 
     def to_mlir(self, operands: List) -> List:
         from mlir.dialects import bufferization
