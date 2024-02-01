918d55a70ced8de8310524437689c3ab1154acc3
Parth Chadha <pchadha@nvidia.com>
2024-02-01 12:51:49 -0800

    Remove incorrect merge

--- tripy/backend/jit/jit.py
+++ tripy/backend/jit/jit.py
@@ -141,7 +141,6 @@ class jit:
             trace_signature_key = hash(tuple(kwargs.items()) + const_tensor_ids + tuple(dim_list))
 
             trace = None
-            input_tensor_info = None
             output_tensor_info = None
 
             if trace_signature_key not in self._trace_signatures:
