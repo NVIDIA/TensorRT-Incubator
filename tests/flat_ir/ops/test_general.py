48802de493a27bdcd05b79d0ac776d0e9052193c
pranavm <pranavm@nvidia.com>
2024-02-02 09:34:39 -0800

    Add tests to ensure ops are implemented as dataclasses with no repr

---
+++ tests/flat_ir/ops/test_general.py
@@ -0,0 +1,23 @@
+import dataclasses
+import inspect
+
+import pytest
+
+from tests import helper
+from tripy.flat_ir.ops import BaseFIROp
+
+OP_TYPES = {obj for obj in helper.discover_tripy_objects() if inspect.isclass(obj) and issubclass(obj, BaseFIROp)}
+
+
+@pytest.mark.parametrize("OpType", OP_TYPES)
+class TestFlatIROps:
+    def test_is_dataclass(self, OpType):
+        assert dataclasses.is_dataclass(
+            OpType
+        ), f"FlatIR ops must be data classes since many base implementations rely on dataclass introspection"
+
+    def test_has_no_dataclass_repr(self, OpType):
+        # If you define a custom repr, add a waiver here.
+        assert (
+            OpType.__repr__ is BaseFIROp.__repr__
+        ), "Use @dataclass(repr=False) to avoid extremely verbose __repr__ implementations"
--- tests/frontend/ops/test_general.py
+++ tests/frontend/ops/test_general.py
@@ -1,7 +1,7 @@
 import inspect
 
 import pytest
-
+import dataclasses
 from tests import helper
 from tripy.frontend.ops import BaseOperator
 
@@ -9,9 +9,21 @@ OP_TYPES = {obj for obj in helper.discover_tripy_objects() if inspect.isclass(ob
 
 
 @pytest.mark.parametrize("OpType", OP_TYPES)
-def test_to_flat_ir_does_not_access_io(OpType):
-    # to_flat_ir() methods should not access `self.inputs` and `self.outputs`
-    source = inspect.getsource(OpType.to_flat_ir)
+class TestFrontendOps:
+    def test_to_flat_ir_does_not_access_io(self, OpType):
+        # to_flat_ir() methods should not access `self.inputs` and `self.outputs`
+        source = inspect.getsource(OpType.to_flat_ir)
 
-    assert "self.inputs" not in source
-    assert "self.outputs" not in source
+        assert "self.inputs" not in source
+        assert "self.outputs" not in source
+
+    def test_is_dataclass(self, OpType):
+        assert dataclasses.is_dataclass(
+            OpType
+        ), f"Frontend ops must be data classes since many base implementations rely on dataclass introspection"
+
+    def test_has_no_dataclass_repr(self, OpType):
+        # If you define a custom repr, add a waiver here.
+        assert (
+            OpType.__repr__ is BaseOperator.__repr__
+        ), "Use @dataclass(repr=False) to avoid extremely verbose __repr__ implementations"
