aaccbe64c96d144a3d3f5ea6e867638a7eea0126
Parth Chadha <pchadha@nvidia.com>
2024-08-02 10:55:49 -0700

    Force the size of slice size to be known at compile time; remove static variant of gather

--- tripy/tripy/flat_ir/ops/gather.py
+++ tripy/tripy/flat_ir/ops/gather.py
@@ -23,39 +23,6 @@ from mlir_tensorrt.compiler.dialects import stablehlo
 
 from tripy.flat_ir.ops.base import BaseFlatIROp
 
-
-@dataclass(repr=False)
-class GatherOp(BaseFlatIROp):
-
-    axis: int
-
-    def to_mlir(self, operands):
-        index_dims = len(self.inputs[1].shape)
-        offset_dims = tuple(
-            list(range(self.axis)) + list(range(self.axis + index_dims, len(self.inputs[0].shape) + index_dims - 1))
-        )
-        index_vector_dim = len(self.inputs[1].shape)
-
-        attr = stablehlo.GatherDimensionNumbers.get(
-            # The set of dimensions in the output shape that offset into an array sliced from operand.
-            offset_dims=offset_dims,
-            # The set of dimensions in each slice that are collapsed away. These dimensions must have size 1.
-            collapsed_slice_dims=(self.axis,),
-            # A map that describes how to map indices in start_indices to legal indices into operand.
-            start_index_map=(self.axis,),
-            # The dimension in start_indices that "contains" the starting indices. See below for a detailed description.
-            index_vector_dim=index_vector_dim,
-        )
-
-        slice_sizes = copy.copy(self.inputs[0].shape)
-        slice_sizes[self.axis] = 1
-        slice_sizes = ir.DenseI64ArrayAttr.get(slice_sizes)
-        gather_out = stablehlo.gather(
-            operand=operands[0], start_indices=operands[1], dimension_numbers=attr, slice_sizes=slice_sizes
-        )
-        return [gather_out]
-
-
 @dataclass(repr=False)
 class DynamicGatherOp(BaseFlatIROp):
 
@@ -63,21 +30,24 @@ class DynamicGatherOp(BaseFlatIROp):
 
     def to_mlir(self, operands):
         index_dims = self.inputs[1].rank
-        offset_dims = tuple(
-            list(range(self.axis)) + list(range(self.axis + index_dims, self.inputs[0].rank + index_dims - 1))
-        )
+        # Ensure slice_sizes is a static tensor with the same shape as the input.
+        operands[2].set_type(ir.RankedTensorType.get([self.inputs[0].rank], operands[2].type.element_type))
+        offset_dims = list(range(self.axis)) + list(range(self.axis + index_dims, self.inputs[0].rank + index_dims - 1))
         index_vector_dim = self.inputs[1].rank
 
         attr = stablehlo.GatherDimensionNumbers.get(
             # The set of dimensions in the output shape that offset into an array sliced from operand.
             offset_dims=offset_dims,
             # The set of dimensions in each slice that are collapsed away. These dimensions must have size 1.
-            collapsed_slice_dims=(self.axis,),
+            collapsed_slice_dims=[self.axis,],
+            operand_batching_dims=[],
             # A map that describes how to map indices in start_indices to legal indices into operand.
-            start_index_map=(self.axis,),
-            # The dimension in start_indices that "contains" the starting indices. See below for a detailed description.
+            start_index_map=[self.axis,],
+            # The dimension in start_indices that "contains" the starting indices.
             index_vector_dim=index_vector_dim,
+            start_indices_batching_dims=[],
         )
+
         gather_out = stablehlo.dynamic_gather(
             operand=operands[0], start_indices=operands[1], dimension_numbers=attr, slice_sizes=operands[2]
         )
