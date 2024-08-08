e991f1285a85bcac3f85cefb1239dd32e3ab7678
Maxim Gluhovskoi <mgluhovskoi@nvidia.com>
2024-08-08 11:49:53 -0700

    Add MLIR test cases for gather

--- tripy/tests/flat_ir/ops/test_gather.py
+++ tripy/tests/flat_ir/ops/test_gather.py
@@ -41,3 +41,16 @@ class TestGatherOp:
             str(gather)
             == f"out: [rank=(3), dtype=(float32), loc=(gpu:0)] = DynamicGatherOp(data, indices, t_inter4, axis={axis})"
         )
+
+    @pytest.mark.parametrize("axis", [0,1])
+    def test_gather_mlir(self, axis):
+        out = tp.gather(tp.Tensor([[1,1,1],[1,1,1]]),axis,tp.Tensor([0],dtype=tp.int32))
+        trace = Trace([out])
+        flat_ir = trace.to_flat_ir()
+        mlir_text = str(flat_ir.to_mlir())
+        if axis == 0:
+            target = '"stablehlo.dynamic_gather"(%c, %c_0, %6) <{dimension_numbers = #stablehlo.gather<offset_dims = [1], collapsed_slice_dims = [0], start_index_map = [0], index_vector_dim = 1>}> : (tensor<2x3xi32>, tensor<1xi32>, tensor<2xi32>) -> tensor<1x?xi32>'
+        else:
+            target = '"stablehlo.dynamic_gather"(%c, %c_0, %6) <{dimension_numbers = #stablehlo.gather<offset_dims = [0], collapsed_slice_dims = [1], start_index_map = [1], index_vector_dim = 1>}> : (tensor<2x3xi32>, tensor<1xi32>, tensor<2xi32>) -> tensor<?x1xi32>'
+        assert target in mlir_text
+        
