9cf2bc6e5a166de54538f956ee6b941956001515
Mark Kraay <mkraay@nvidia.com>
2024-08-13 11:07:28 -0700

    enable test_full: failing test_shape_tensor[compile]

--- tripy/tests/integration/test_full.py
+++ tripy/tests/integration/test_full.py
@@ -20,18 +20,20 @@ import numpy as np
 
 import tripy as tp
 
+tp.logger.verbosity = "ir"
+
 
 class TestFull:
-    def test_normal_shape(self):
-        out = tp.full((2, 2), 5.0, tp.float32)
+    def test_normal_shape(self, compile_fixture):
+        out = compile_fixture(tp.full, (2, 2), 5.0, tp.float32)
         assert np.array_equal(cp.from_dlpack(out).get(), np.full((2, 2), 5.0, np.float32))
 
-    def test_shape_tensor(self):
+    def test_shape_tensor(self, compile_fixture):
         a = tp.ones((2, 3))
-        out = tp.full(a.shape, 5.0, tp.float32)
+        out = compile_fixture(tp.full, a.shape, 5.0, tp.float32)
         assert np.array_equal(cp.from_dlpack(out).get(), np.full((2, 3), 5.0, np.float32))
 
-    def test_mixed_shape(self):
+    def test_mixed_shape(self, compile_fixture):
         a = tp.ones((2, 3))
-        out = tp.full((a.shape[0], 4), 5.0, tp.float32)
+        out = compile_fixture(tp.full, (a.shape[0], 4), 5.0, tp.float32)
         assert np.array_equal(cp.from_dlpack(out).get(), np.full((2, 4), 5.0, np.float32))
