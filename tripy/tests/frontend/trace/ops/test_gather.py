2a60f55affd8222b5aac228660bfbc3b0e26bfdb
Mgluhovskoi <Mgluhovskoi@nvidia.com>
2024-08-14 15:28:11 -0500

    Add integration/IR test cases for gather (#67)
    Addresses Issue #52.

--- tripy/tests/frontend/trace/ops/test_gather.py
+++ tripy/tests/frontend/trace/ops/test_gather.py
@@ -24,26 +24,29 @@ from tripy.frontend.trace.ops import Gather
 
 
 class TestGather:
-    def test_op_func_all_partial(self):
-        a = tp.Tensor([1, 2, 3, 4])
+    @pytest.mark.parametrize("axis", [0, 1, 2])
+    def test_op_func_all_partial(self, axis):
+        a = tp.Tensor([[[1, 2, 3, 4], [1, 2, 3, 4]], [[1, 2, 3, 4], [1, 2, 3, 4]]])
         index = tp.Tensor(np.zeros(1, dtype=np.int32))
-        out = tp.gather(a, 0, index)
+        out = tp.gather(a, axis, index)
         assert isinstance(out, tp.Tensor)
         assert isinstance(out.trace_tensor.producer, Gather)
 
-    def test_incorrect_dtype(self):
-        a = tp.Tensor([[1, 2], [3, 4]], shape=(2, 2))
+    @pytest.mark.parametrize("axis", [0, 1, 2])
+    def test_incorrect_dtype(self, axis):
+        a = tp.Tensor([[[1, 2, 3, 4], [1, 2, 3, 4]], [[1, 2, 3, 4], [1, 2, 3, 4]]])
         index = tp.Tensor(np.zeros(1, dtype=np.float32))
         with helper.raises(
             tp.TripyException,
             match="Index tensor for gather operation should be of int32 type.",
             has_stack_info_for=[a, index],
         ):
-            b = tp.gather(a, 0, index)
+            b = tp.gather(a, axis, index)
 
     @pytest.mark.parametrize("index_shape", [(1,), (2, 2)])
-    def test_infer_rank(self, index_shape):
-        a = tp.Tensor([1, 2, 3, 4])
+    @pytest.mark.parametrize("axis", [0, 1, 2])
+    def test_infer_rank(self, index_shape, axis):
+        a = tp.Tensor([[[1, 2, 3, 4], [1, 2, 3, 4]], [[1, 2, 3, 4], [1, 2, 3, 4]]])
         index = tp.Tensor(np.zeros(index_shape, dtype=np.int32))
-        out = tp.gather(a, 0, index)
+        out = tp.gather(a, axis, index)
         assert out.trace_tensor.rank == a.rank + len(index_shape) - 1
