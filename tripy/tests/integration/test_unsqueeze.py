b2b87f84260e63022ab1a7d9908e8283251d27ef
pranavm <pranavm@nvidia.com>
2024-08-30 13:59:36 -0700

    Updates unsqueeze test to check that shape is correct

--- tripy/tests/integration/test_unsqueeze.py
+++ tripy/tests/integration/test_unsqueeze.py
@@ -23,16 +23,15 @@ import tripy as tp
 
 
 class TestUnsqueezeOp:
-    @pytest.mark.parametrize("axis", [0, 2, 3])
+    @pytest.mark.parametrize("axis", [-1, 0, 2])
     def test_unsqueeze_dynamic_op(self, axis):
         def func(a):
             return tp.unsqueeze(a, dim=axis)
 
-        # TODO: DS blocked on mlir-tensorrt #635
-        # compiler = tp.Compiler(func)
-        # compiler.compile(tp.InputInfo(([2, 4, 6], 2, 2, 3), dtype=tp.float32))
-
         inp = np.ones((4, 2, 2, 3), dtype=np.float32)
 
         out = func(tp.Tensor(inp))
-        assert tp.allclose(out, tp.Tensor(np.expand_dims(inp, axis=axis)))
+        ref_out = np.expand_dims(inp, axis=axis)
+        assert tp.allclose(out, tp.Tensor(ref_out))
+
+        assert out.shape == ref_out.shape
