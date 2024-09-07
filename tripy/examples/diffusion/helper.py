ce327ad05af78c61bd37b2aef12a769851eecba5
Akhil Goel <akhilg@nvidia.com>
2024-09-06 19:15:20 -0700

    Add fp16 support
    Signed-off-by: Akhil Goel <akhilg@nvidia.com>

--- tripy/examples/diffusion/helper.py
+++ tripy/examples/diffusion/helper.py
@@ -12,6 +12,7 @@ def scaled_dot_product_attention(
     embedding_dim: Optional[int] = None,
     attn_mask: Optional[tp.Tensor] = None,
     is_causal: bool = False,
+    dtype: tp.dtype = tp.float16
 ) -> tp.Tensor:
     """
     Computes scaled dot-product attention.
@@ -20,14 +21,15 @@ def scaled_dot_product_attention(
     - Described: https://paperswithcode.com/method/scaled
     - Paper: https://arxiv.org/abs/1706.03762v7
     """
-
     if is_causal:  # this path is not called in demoDiffusion
         target_shape = query.shape[-2:-1] + key.shape[-2:-1]
         # TODO: #228: WAR to prevent computing output rank in infer_rank for reshape
         target_shape.trace_tensor.shape = (2,)
         attn_mask = tp.cast(tp.tril(tp.ones(target_shape)), tp.bool)
     if attn_mask is not None and attn_mask.dtype == tp.bool:
-        attn_mask = tp.where((attn_mask == 0), tp.ones_like(attn_mask) * -float("inf"), tp.zeros_like(attn_mask))
+        attn_mask = tp.where((attn_mask == 0), tp.ones_like(attn_mask, dtype=dtype) * -float("inf"), tp.zeros_like(attn_mask, dtype=dtype))
+    if attn_mask is not None:
+        attn_mask = tp.cast(attn_mask, dtype)
     qk = query @ tp.transpose(key, -2, -1) / math.sqrt(embedding_dim)
     return tp.cast(tp.softmax((qk + attn_mask) if attn_mask is not None else qk, -1), query.dtype) @ value
 
