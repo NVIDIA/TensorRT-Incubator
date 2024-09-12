Fix NaN bug in normalization for fp16

tripy/examples/diffusion/model.py
@@ -33,7 +33,7 @@ from examples.diffusion.helper import clamp
 
 @dataclass
 class StableDiffusionConfig:
-    dtype: tp.dtype = tp.float16
+    dtype: tp.dtype = tp.float32
     clip_config: Optional[CLIPConfig] = field(default=None, init=False)
     unet_config: Optional[UNetConfig] = field(default=None, init=False)
     vae_config: Optional[VAEConfig] = field(default=None, init=False)
@@ -44,11 +44,11 @@ class StableDiffusionConfig:
         self.vae_config = VAEConfig(dtype=self.dtype)
 
 # equivalent to LMSDiscreteScheduler(beta_start=0.00085, beta_end=0.012, beta_schedule="scaled_linear", num_train_timesteps=1000)
-def get_alphas_cumprod(beta_start=0.00085, beta_end=0.0120, n_training_steps=1000):
+def get_alphas_cumprod(beta_start=0.00085, beta_end=0.0120, n_training_steps=1000, dtype=tp.float32):
     betas = np.linspace(beta_start**0.5, beta_end**0.5, n_training_steps, dtype=np.float32) ** 2
     alphas = 1.0 - betas
     alphas_cumprod = np.cumprod(alphas, axis=0)
-    return tp.Tensor(alphas_cumprod)
+    return tp.cast(tp.Tensor(alphas_cumprod), dtype)
 
 
 class StableDiffusion(tp.Module):