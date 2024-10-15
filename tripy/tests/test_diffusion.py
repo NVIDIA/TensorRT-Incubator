7225a1b71be7324c2e1529d83f56c7b5b022aa82
Akhil Goel <akhilg@nvidia.com>
2024-10-14 17:38:26 -0700

    Update image saving and test

--- tripy/tests/test_diffusion.py
+++ tripy/tests/test_diffusion.py
@@ -1,3 +1,17 @@
+# SPDX-FileCopyrightText: Copyright (c) 2024 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 import pytest
 import torch
 import numpy as np
@@ -23,22 +37,31 @@ def check_equal(tp_array, torch_tensor, dtype=torch.float32, debug=False):
         rel_diff = torch.abs(diff) / (torch.abs(b) + eps)
         max_rel_diff = torch.max(rel_diff)
         print(f"Maximum relative difference: {max_rel_diff}\n")
-        
-    assert torch.allclose(torch.from_dlpack(tp_array).to(dtype), torch_tensor.to(dtype)), f"\nTP Array:\n {tp_array} \n!= Torch Tensor:\n {torch_tensor}"
+
+    assert torch.allclose(
+        torch.from_dlpack(tp_array).to(dtype), torch_tensor.to(dtype)
+    ), f"\nTP Array:\n {tp_array} \n!= Torch Tensor:\n {torch_tensor}"
+
 
 @pytest.mark.l1
 class TestConvolution:
     def test_ssim(self):
-        args = Namespace(steps=50, prompt='a beautiful photograph of Mt. Fuji during cherry blossom', out='output/rendered.png', fp16=False, seed=100, guidance=7.5, torch_inference=False)
+        args = Namespace(
+            steps=50,
+            prompt="a beautiful photograph of Mt. Fuji during cherry blossom",
+            out="temp.png",
+            fp16=True,
+            seed=420,
+            guidance=7.5,
+            torch_inference=False,
+            hf_token="",
+            engine_dir="diffusion_engines",
+        )
         tp_img, _ = tripy_diffusion(args)
-        print(f"first: {tp_img}")
-        tp_img = np.array(tp_img.convert('L'))
-        print(f"second: {tp_img}")
+        tp_img = np.array(tp_img.convert("L"))
+
         torch_img, _ = hf_diffusion(args)
-        print(f"third: {torch_img}")
-        torch_img = np.array(torch_img.convert('L'))
-        print(f"fourth: {torch_img}")
+        torch_img = np.array(torch_img.convert("L"))
 
         ssim = structural_similarity(tp_img, torch_img)
-        print(f"SSIM IS: {ssim}")
-        assert ssim >= 0.85, "Structural Similarity score expected >= 0.85 but got {ssim}"
+        assert ssim >= 0.80, f"Structural Similarity score expected >= 0.80 but got {ssim}"
