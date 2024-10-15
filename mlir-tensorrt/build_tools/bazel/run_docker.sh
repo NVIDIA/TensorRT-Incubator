752e47956bc58cb9b64bdcfeb43497965e74d371
Sambhav Jain <sambhav.jain@getcruise.com>
2024-10-15 15:54:12 -0700

    docker for bazel

---
+++ mlir-tensorrt/build_tools/bazel/run_docker.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+docker build -f build_tools/bazel/Dockerfile \
+             -t mlir-tensorrt:dev \
+			 --build-arg BASE_IMAGE=nvcr.io/nvidia/cuda:12.5.1-cudnn-devel-ubuntu22.04 \
+             --build-arg GROUP=$(id -gn) \
+             --build-arg GID=$(id -g) \
+             --build-arg USER=$(id -un) \
+             --build-arg UID=$(id -u) \
+             .
+
+docker run -it \
+           -v "$(pwd)":"/opt/src/mlir-tensorrt" \
+           mlir-tensorrt:dev
