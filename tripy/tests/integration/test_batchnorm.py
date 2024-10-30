a5b1b06e07fa9a0bb3daed302fbd9c7dcb0072b4
Faraz <58580514+farazkh80@users.noreply.github.com>
2024-10-30 14:47:37 -0400

    [tripy] Batchnorm implementation (#319)
    Batchnorm implementation, currently be used in ResNet50 notebook PR,
    adding it as a general module similar to `LayerNorm`.
    
    Example
    ```python
    batch_norm = tp.BatchNorm(3)
    
    input = tp.iota((2, 3, 1, 1))
    output = batch_norm(input)
    ```
    
    
    Tested the code and documnation.
    
    ---------
    
    Signed-off-by: Faraz Khoubsirat <farazkhoubsirat80@gmail.com>
    Signed-off-by: Faraz <58580514+farazkh80@users.noreply.github.com>
    Co-authored-by: Faraz Khoubsirat <fkhoubsirat@computelab-304.nvidia.com>
    Co-authored-by: pranavm-nvidia <49246958+pranavm-nvidia@users.noreply.github.com>
    Co-authored-by: Sagar Shelke <sagshelke@nvidia.com>

---
+++ tripy/tests/integration/test_batchnorm.py
@@ -0,0 +1,67 @@
+# SPDX-FileCopyrightText: Copyright (c) 2024 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import torch
+import pytest
+import re
+
+import tripy as tp
+from tests import helper
+
+DTYPES = [(torch.float32, tp.float32), (torch.float16, tp.float16)]
+
+
+class TestBatchNorm:
+
+    @pytest.mark.parametrize("torch_dtype, tp_dtype", DTYPES)
+    @pytest.mark.parametrize("input_shape", [(2, 2, 2, 2)])
+    def test_batchnorm_accuracy(self, torch_dtype, tp_dtype, input_shape):
+        eps = 1e-5
+        num_features = input_shape[1]  # Number of channels in the input tensor
+        batchnorm = torch.nn.BatchNorm2d(num_features=num_features, eps=eps, dtype=torch_dtype)
+        tp_batchnorm = tp.BatchNorm(
+            num_features=num_features,
+            dtype=tp_dtype,
+            eps=eps,
+        )
+
+        # Use Tripy's parameters and ensure they match the dtype
+        tp_batchnorm.weight = tp.Parameter(batchnorm.weight.detach())
+        tp_batchnorm.bias = tp.Parameter(batchnorm.bias.detach())
+        tp_batchnorm.running_mean = tp.Parameter(batchnorm.running_mean.detach())
+        tp_batchnorm.running_var = tp.Parameter(batchnorm.running_var.detach())
+
+        input = torch.randn(input_shape, dtype=torch_dtype).to("cuda")
+        tp_input = tp.Tensor(input, dtype=tp_dtype)
+
+        output = tp_batchnorm(tp_input)
+
+        batchnorm.to("cuda").eval()
+        with torch.no_grad():
+            expected = batchnorm(input)
+
+        rtol_ = 2e-7 if tp_dtype == tp.float32 else 1e-3
+        assert torch.allclose(torch.from_dlpack(output), expected, rtol=rtol_)
+
+    def test_batchnorm_improper_dimensions(self):
+        num_features = 2
+        tp_batchnorm = tp.BatchNorm(
+            num_features=num_features,
+        )
+        x = tp.ones((3, 3, 3))
+        with helper.raises(
+            tp.TripyException,
+            match=re.escape("size of operand dimension 1 (2) is not compatible with size of result dimension 1 (3)"),
+        ):
+            tp_batchnorm(x).eval()
