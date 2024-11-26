91639443b367df62399eb10958948427959f7d9e
Faraz Khoubsirat <farazkhoubsirat80@gmail.com>
2024-11-26 19:34:44 +0000

    -S
    fix and remove some files

--- tripy/tests/performance/cases/__init__.py
+++ tripy/tests/performance/cases/__init__.py
@@ -38,7 +38,6 @@ def __discover_modules():
                     for submod in pkgutil.iter_modules(mod.__path__)
                 ]
             )
-            print(mods)
 
 
 modules = list(__discover_modules())[1:]
