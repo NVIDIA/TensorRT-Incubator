825adee96ccdfb960979433b79be554cfe3ae5b0
Parth Chadha <pchadha@nvidia.com>
2024-12-11 12:43:54 -0800

    Add missing video_demo file

---
+++ tripy/examples/segment-anything-model-v2/video_demo.py
@@ -0,0 +1,180 @@
+# SPDX-FileCopyrightText: Copyright (c) 2024 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import argparse
+import torch
+from sam2.build_sam import build_sam2_video_predictor
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image
+from demo_utils import process_and_show_mask, show_box, show_points
+
+import os
+import time
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--use_tripy", action="store_true", help="use tripy backbone config file")
+args = parser.parse_args()
+
+
+def show_mask(mask, ax, obj_id=None, random_color=False):
+    if random_color:
+        color = np.concatenate([np.random.random(3), np.array([0.6])], axis=0)
+    else:
+        cmap = plt.get_cmap("tab10")
+        cmap_idx = 0 if obj_id is None else obj_id
+        color = np.array([*cmap(cmap_idx)[:3], 0.6])
+    h, w = mask.shape[-2:]
+    mask_image = mask.reshape(h, w, 1) * color.reshape(1, 1, -1)
+    ax.imshow(mask_image)
+
+
+sam2_checkpoint = "./checkpoints/sam2.1_hiera_large.pt"
+model_cfg = "sam2_hiera_l.yaml"
+
+predictor = build_sam2_video_predictor(model_cfg, sam2_checkpoint, device=torch.device("cuda"))
+
+
+# `video_dir` a directory of JPEG frames with filenames like `<frame_index>.jpg`
+video_dir = "./bedroom"
+
+# scan all the JPEG frame names in this directory
+frame_names = [p for p in os.listdir(video_dir) if os.path.splitext(p)[-1] in [".jpg", ".jpeg", ".JPG", ".JPEG"]]
+frame_names.sort(key=lambda p: int(os.path.splitext(p)[0]))
+# take a look the first video frame
+frame_idx = 0
+plt.figure(figsize=(9, 6))
+plt.title(f"frame {frame_idx}")
+plt.imshow(Image.open(os.path.join(video_dir, frame_names[frame_idx])))
+plt.savefig(f"video_{frame_idx}.png")
+
+
+inference_state = predictor.init_state(video_path=video_dir)
+
+
+def make_tensors_contiguous(d):
+    for key, value in d.items():
+        if isinstance(value, torch.Tensor):
+            d[key] = value.contiguous()
+    return d
+
+
+inference_state = make_tensors_contiguous(inference_state)
+
+predictor.reset_state(inference_state)
+
+prompts = {}  # hold all the clicks we add for visualization
+
+ann_frame_idx = 0  # the frame index we interact with
+ann_obj_id = 2  # give a unique id to each object we interact with (it can be any integers)
+
+# Let's add a positive click at (x, y) = (200, 300) to get started on the first object
+points = np.array([[200, 300]], dtype=np.float32)
+# for labels, `1` means positive click and `0` means negative click
+labels = np.array([1], np.int32)
+prompts[ann_obj_id] = points, labels
+_, out_obj_ids, out_mask_logits = predictor.add_new_points_or_box(
+    inference_state=inference_state,
+    frame_idx=ann_frame_idx,
+    obj_id=ann_obj_id,
+    points=points,
+    labels=labels,
+)
+
+# show the results on the current (interacted) frame
+plt.figure(figsize=(9, 6))
+plt.title(f"frame {ann_frame_idx}")
+plt.imshow(Image.open(os.path.join(video_dir, frame_names[ann_frame_idx])))
+show_points(points, labels, plt.gca())
+for i, out_obj_id in enumerate(out_obj_ids):
+    show_points(*prompts[out_obj_id], plt.gca())
+    show_mask((out_mask_logits[i] > 0.0).cpu().numpy(), plt.gca(), obj_id=out_obj_id)
+
+# add the first object
+ann_frame_idx = 0  # the frame index we interact with
+ann_obj_id = 2  # give a unique id to each object we interact with (it can be any integers)
+
+# Let's add a 2nd negative click at (x, y) = (275, 175) to refine the first object
+# sending all clicks (and their labels) to `add_new_points_or_box`
+points = np.array([[200, 300], [275, 175]], dtype=np.float32)
+# for labels, `1` means positive click and `0` means negative click
+labels = np.array([1, 0], np.int32)
+prompts[ann_obj_id] = points, labels
+_, out_obj_ids, out_mask_logits = predictor.add_new_points_or_box(
+    inference_state=inference_state,
+    frame_idx=ann_frame_idx,
+    obj_id=ann_obj_id,
+    points=points,
+    labels=labels,
+)
+
+# show the results on the current (interacted) frame
+plt.figure(figsize=(9, 6))
+plt.title(f"frame {ann_frame_idx}")
+plt.imshow(Image.open(os.path.join(video_dir, frame_names[ann_frame_idx])))
+show_points(points, labels, plt.gca())
+for i, out_obj_id in enumerate(out_obj_ids):
+    show_points(*prompts[out_obj_id], plt.gca())
+    show_mask((out_mask_logits[i] > 0.0).cpu().numpy(), plt.gca(), obj_id=out_obj_id)
+
+
+ann_frame_idx = 0  # the frame index we interact with
+ann_obj_id = 3  # give a unique id to each object we interact with (it can be any integers)
+
+# Let's now move on to the second object we want to track (giving it object id `3`)
+# with a positive click at (x, y) = (400, 150)
+points = np.array([[400, 150]], dtype=np.float32)
+# for labels, `1` means positive click and `0` means negative click
+labels = np.array([1], np.int32)
+prompts[ann_obj_id] = points, labels
+
+# `add_new_points_or_box` returns masks for all objects added so far on this interacted frame
+_, out_obj_ids, out_mask_logits = predictor.add_new_points_or_box(
+    inference_state=inference_state,
+    frame_idx=ann_frame_idx,
+    obj_id=ann_obj_id,
+    points=points,
+    labels=labels,
+)
+
+# show the results on the current (interacted) frame on all objects
+plt.figure(figsize=(9, 6))
+plt.title(f"frame {ann_frame_idx}")
+plt.imshow(Image.open(os.path.join(video_dir, frame_names[ann_frame_idx])))
+show_points(points, labels, plt.gca())
+for i, out_obj_id in enumerate(out_obj_ids):
+    show_points(*prompts[out_obj_id], plt.gca())
+    show_mask((out_mask_logits[i] > 0.0).cpu().numpy(), plt.gca(), obj_id=out_obj_id)
+
+
+# run propagation throughout the video and collect the results in a dict
+start = time.perf_counter()
+video_segments = {}  # video_segments contains the per-frame segmentation results
+for out_frame_idx, out_obj_ids, out_mask_logits in predictor.propagate_in_video(inference_state):
+    video_segments[out_frame_idx] = {
+        out_obj_id: (out_mask_logits[i] > 0.0).cpu().numpy() for i, out_obj_id in enumerate(out_obj_ids)
+    }
+end = time.perf_counter()
+print(f"Video segmentation took {(end - start)}s")
+
+# render the segmentation results every few frames
+vis_frame_stride = 30
+plt.close("all")
+for out_frame_idx in range(0, len(frame_names), vis_frame_stride):
+    plt.figure(figsize=(6, 4))
+    plt.title(f"frame {out_frame_idx}")
+    plt.imshow(Image.open(os.path.join(video_dir, frame_names[out_frame_idx])))
+    for out_obj_id, out_mask in video_segments[out_frame_idx].items():
+        show_mask(out_mask, plt.gca(), obj_id=out_obj_id)
+    plt.savefig(f"video_final_mask_{out_frame_idx}.png")
