ba18e29561229a45e8e0360f7d19d68bcb34914b
Faraz <58580514+farazkh80@users.noreply.github.com>
2024-12-18 19:22:54 -0500

    Update tripy/nvtripy/frontend/trace/ops/pooling.py
    Co-authored-by: pranavm-nvidia <49246958+pranavm-nvidia@users.noreply.github.com>
    Signed-off-by: Faraz <58580514+farazkh80@users.noreply.github.com>

--- tripy/nvtripy/frontend/trace/ops/pooling.py
+++ tripy/nvtripy/frontend/trace/ops/pooling.py
@@ -200,7 +200,8 @@ def maxpool(
 @export.public_api(document_under="operations/functions")
 @wrappers.interface(
     dtype_constraints={"input": "T1", wrappers.RETURN_VALUE: "T1"},
-    dtype_variables={"T1": ["float32", "float16"]},  # TODO: Add int8 once MLIR-TRT supports (issue #457)
+    # TODO (#457): Add int8 once MLIR-TRT supports
+    dtype_variables={"T1": ["float32", "float16"]},  
 )
 def avgpool(
     input: "nvtripy.Tensor",
