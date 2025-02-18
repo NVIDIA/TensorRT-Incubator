1b03595016b8b64496be8d1db42159f710602d8e
pranavm <pranavm@nvidia.com>
2025-02-18 10:25:36 -0800

    Deletes unary FlatIR ops

--- tripy/nvtripy/flat_ir/ops/__init__.py
+++ tripy/nvtripy/flat_ir/ops/__init__.py
@@ -1,5 +1,5 @@
 #
-# SPDX-FileCopyrightText: Copyright (c) 2024 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -15,7 +15,6 @@
 # limitations under the License.
 #
 
-from nvtripy.flat_ir.ops.abs import AbsOp
 from nvtripy.flat_ir.ops.add import AddOp
 from nvtripy.flat_ir.ops.base import BaseFlatIROp
 from nvtripy.flat_ir.ops.broadcast import DynamicBroadcastOp
@@ -26,16 +25,13 @@ from nvtripy.flat_ir.ops.constant import ConstantOp
 from nvtripy.flat_ir.ops.convert import ConvertOp
 from nvtripy.flat_ir.ops.convolution import ConvolutionOp
 from nvtripy.flat_ir.ops.copy import CopyOp
-from nvtripy.flat_ir.ops.cos import CosineOp
 from nvtripy.flat_ir.ops.divide import DivideOp
 from nvtripy.flat_ir.ops.dot import DotOp
-from nvtripy.flat_ir.ops.exponential import ExpOp
 from nvtripy.flat_ir.ops.flip import FlipOp
 from nvtripy.flat_ir.ops.floor import FloorOp
 from nvtripy.flat_ir.ops.gather import DynamicGatherOp
 from nvtripy.flat_ir.ops.get_dimension_size import GetDimensionSizeOp
 from nvtripy.flat_ir.ops.iota import DynamicIotaOp
-from nvtripy.flat_ir.ops.log import LogOp
 from nvtripy.flat_ir.ops.maximum import MaxOp
 from nvtripy.flat_ir.ops.minimum import MinOp
 from nvtripy.flat_ir.ops.mul import MulOp
@@ -47,11 +43,7 @@ from nvtripy.flat_ir.ops.reduce_window import ReduceWindowOp
 from nvtripy.flat_ir.ops.resize import ResizeCubicOp, ResizeLinearOp, ResizeNearestOp
 from nvtripy.flat_ir.ops.reshape import DynamicReshapeOp
 from nvtripy.flat_ir.ops.round_nearest_even import RoundNearestEvenOp
-from nvtripy.flat_ir.ops.rsqrt import RsqrtOp
 from nvtripy.flat_ir.ops.select import SelectOp
-from nvtripy.flat_ir.ops.sin import SineOp
 from nvtripy.flat_ir.ops.slice import DynamicSliceOp
-from nvtripy.flat_ir.ops.sqrt import SqrtOp
 from nvtripy.flat_ir.ops.sub import SubtractOp
-from nvtripy.flat_ir.ops.tanh import TanhOp
 from nvtripy.flat_ir.ops.transpose import TransposeOp
