5dd8cb1101aceb1074505212fafb4b3406fa478a
pranavm <pranavm@nvidia.com>
2025-02-20 11:18:28 -0800

    Ports concatenate to TRT dialect, fixes shape inference bugs
    - Ports `concatenate` to TRT dialect.
    
    - Fixes a bug where shapes of shape tensors were not being propagated correctly
         to MLIR.

--- tripy/nvtripy/frontend/ops/zeros.py
+++ tripy/nvtripy/frontend/ops/zeros.py
@@ -1,3 +1,17 @@
+# SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 from typing import Optional
 
 from nvtripy import export
@@ -36,7 +50,7 @@ def zeros(
 
     .. seealso:: :func:`zeros_like`, :func:`full`
     """
-    return full(shape, 0, dtype)
+    return full(shape, 0.0, dtype)
 
 
 @export.public_api(document_under="operations/initializers")
@@ -69,4 +83,4 @@ def zeros_like(input: "nvtripy.Tensor", dtype: Optional[datatype.dtype] = None) 
     .. seealso:: :func:`zeros`, :func:`full_like`
     """
 
-    return full_like(input, 0, dtype)
+    return full_like(input, 0.0, dtype)
