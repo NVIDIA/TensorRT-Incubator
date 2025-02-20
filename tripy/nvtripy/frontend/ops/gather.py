4b760d265f1282338416264b78b3a92f7a16a736
pranavm <pranavm@nvidia.com>
2025-02-20 14:56:17 -0800

    Ports `gather` to TRT dialect

--- tripy/nvtripy/frontend/ops/gather.py
+++ tripy/nvtripy/frontend/ops/gather.py
@@ -1,5 +1,5 @@
 #
-# SPDX-FileCopyrightText: Copyright (c) 2024 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -30,19 +30,20 @@ from nvtripy.utils import wrappers
         "T2": ["int32"],
     },
 )
+# TODO (pranavm): Document this better (+example) for the >1D indices case - also add tests.
 def gather(input: "nvtripy.Tensor", dim: int, index: "nvtripy.Tensor") -> "nvtripy.Tensor":
     """
     Gather values from the input tensor along the specified axis based on the specified indices.
     This behaves similarly to ``numpy.take()``.
 
     Args:
-        input: The input tensor
-        dim: Axis along which data is gathered.
-        index: The indices of elements to gather.
+        input: The input tensor.
+        dim: The dimension along which to gather.
+        index: A tensor of indices to gather.
 
     Returns:
         A new tensor of the same shape along every
-        dimension except ``dim``, which will have a size equal to ``len(index)``.
+        dimension except ``dim``, which will have a size equal to ``index.shape[0]``.
 
     .. code-block:: python
         :linenos:
@@ -53,4 +54,7 @@ def gather(input: "nvtripy.Tensor", dim: int, index: "nvtripy.Tensor") -> "nvtri
 
         assert np.array_equal(cp.from_dlpack(output).get(), np.take(cp.from_dlpack(data).get(), cp.from_dlpack(indices).get(), axis=1))
     """
+    # TODO (pranavm): Test negative axis?
+    dim = op_utils.process_dim(dim, input.rank)
+
     return op_utils.create_op(Gather, [input, index], dim)
