64a84bd43c37c81ee22256f42d13d6f9eb65acb5
pranavm <pranavm@nvidia.com>
2025-02-20 11:18:28 -0800

    Ports concatenate to TRT dialect, fixes shape inference bugs
    - Ports `concatenate` to TRT dialect.
    
    - Fixes a bug where shapes of shape tensors were not being propagated correctly
         to MLIR.

--- tripy/nvtripy/trace/ops/concatenate.py
+++ tripy/nvtripy/trace/ops/concatenate.py
@@ -1,5 +1,5 @@
 #
-# SPDX-FileCopyrightText: Copyright (c) 2024 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -18,6 +18,7 @@
 from dataclasses import dataclass
 
 import nvtripy.trace.ops.utils as op_utils
+from mlir_tensorrt.compiler.dialects import tensorrt
 from nvtripy.trace.ops.base import BaseTraceOp
 
 
@@ -25,14 +26,18 @@ from nvtripy.trace.ops.base import BaseTraceOp
 class Concatenate(BaseTraceOp):
     dim: int
 
-    infer_rank = op_utils.InferRankPolicies.max_of_inputs()
+    def infer_rank(self):
+        assert len(set(inp.rank for inp in self.inputs)) == 1, "All inputs must have the same rank!"
+        return op_utils.InferRankPolicies.same_as_input()(self)
 
     def infer_devices(self):
         self.outputs[0].device = self.inputs[0].device
 
-    def to_flat_ir(self, inputs, outputs):
-        from nvtripy.flat_ir.ops import ConcatenateOp
+    def to_mlir(self, inputs, outputs):
+        output = tensorrt.concatenation(inputs, axis=self.dim)
+        # TODO (pranavm): See if we want to do this type of overwriting at the call-site.
+        # We can decide when to overwrite based on what has more information.
 
-        if self.dim < 0:
-            self.dim += inputs[0].rank
-        ConcatenateOp.build(inputs, outputs, dim=self.dim)
+        # Overwrite output type so shape propagation works.
+        output.set_type(outputs[0])
+        return [output]
