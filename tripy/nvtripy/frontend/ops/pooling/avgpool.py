e1a6d7aba061eaef3c80d33340717ecbd7b7d217
Yizhuo Zhang <yizhuoz@nvidia.com>
2025-02-24 15:11:40 -0800

    Port pooling op

--- tripy/nvtripy/frontend/ops/pooling/avgpool.py
+++ tripy/nvtripy/frontend/ops/pooling/avgpool.py
@@ -18,9 +18,10 @@
 from typing import Optional, Sequence
 
 from nvtripy import export, utils
+from nvtripy.common import datatype
 from nvtripy.common.exception import raise_error
 from nvtripy.frontend.ops import utils as op_utils
-from nvtripy.trace.ops.pooling import Pooling
+from nvtripy.trace.ops.pooling import AvgPooling
 from nvtripy.utils import wrappers
 
 
@@ -29,70 +30,12 @@ from nvtripy.utils import wrappers
     dtype_constraints={"input": "T1", wrappers.RETURN_VALUE: "T1"},
     dtype_variables={"T1": ["float32", "float16", "int8"]},
 )
-def maxpool(
-    input: "nvtripy.Tensor",
-    kernel_dims: Sequence[int],
-    stride: Optional[Sequence[int]] = None,
-    padding: Optional[Sequence[Sequence[int]]] = None,
-) -> "nvtripy.Tensor":
-    r"""
-    Applies a max pooling over the input tensor.
-
-    The output's non-spatial dimensions are the same as input. For each input spatial dimension
-    :math:`D_{i}`, the corresponding output dimension will be:
-
-    .. math::
-        D_{out_i} = \left\lfloor\frac{D_{i} + \text{padding_before[i]} + \text{padding_after[i]} -
-                \text{kernel_dims[i]}}{\text{stride[i]}} + 1\right\rfloor
-
-    Args:
-        input: The input tensor.
-        kernel_dims: The spatial shape of the pooling window. Only 2-D or 3-D ``kernel_dims`` are supported.
-            If the input has :class:`int8` datatype, ``kernel_dims`` can only be 2-D.
-        stride: A sequence of length :math:`M` indicating the stride of pooling across each spatial dimension,
-            where :math:`M` is the number of spatial dimensions, i.e. :math:`M = \text{rank(input)} - 2`.
-            Defaults to all 1.
-        padding: A sequence of pairs of integers of length :math:`M` indicating the zero padding
-            to apply to the input along each spatial dimension before and after the dimension respectively,
-            where :math:`M` is the number of spatial dimensions, i.e. :math:`M = \text{rank(input)} - 2`.
-            Defaults to all 0.
-
-    Returns:
-        The result tensor after the pooling operation.
-
-    .. code-block:: python
-        :linenos:
-
-        input = tp.reshape(tp.arange(16, dtype=tp.float32), (1, 1, 4, 4))
-        output = tp.maxpool(input, kernel_dims=(2, 2))
-
-        pool_torch = torch.nn.MaxPool2d((2, 2), stride=1) # doc: omit
-        expected = pool_torch(torch.from_dlpack(input).to("cpu")) # doc: omit
-
-        assert torch.allclose(torch.from_dlpack(output).to("cpu"), expected)
-    """
-    spatial_dims = len(kernel_dims)
-    if spatial_dims != 2 and spatial_dims != 3:
-        raise_error("Unsupported kernel_dims, must be 2D or 3D.", [f"Got kernel_dims={kernel_dims}"])
-
-    op_utils.check_conv_pooling_args(kernel_dims, stride, padding)
-    stride = utils.utils.default(stride, [1] * spatial_dims)
-    padding = utils.utils.default(padding, [(0, 0)] * spatial_dims)
-
-    return op_utils.create_op(Pooling, [input], Pooling.Kind.MAX, kernel_dims, stride, padding)
-
-
-@export.public_api(document_under="operations/functions")
-@wrappers.interface(
-    dtype_constraints={"input": "T1", wrappers.RETURN_VALUE: "T1"},
-    # TODO (#457): Add int8 once MLIR-TRT supports
-    dtype_variables={"T1": ["float32", "float16"]},
-)
 def avgpool(
     input: "nvtripy.Tensor",
     kernel_dims: Sequence[int],
     stride: Optional[Sequence[int]] = None,
     padding: Optional[Sequence[Sequence[int]]] = None,
+    count_include_pad: bool = True,
 ) -> "nvtripy.Tensor":
     r"""
     Applies an average pooling over the input tensor.
@@ -115,6 +58,8 @@ def avgpool(
             to apply to the input along each spatial dimension before and after the dimension respectively,
             where :math:`M` is the number of spatial dimensions, i.e. :math:`M = \text{rank(input)} - 2`.
             Defaults to all 0.
+        count_include_pad: When True, the padded values will be included in the averaging operation. This
+            option can only be ``True`` when input datatype is :class:`int8`.
 
     Returns:
         The result tensor after the pooling operation.
@@ -135,7 +80,20 @@ def avgpool(
         raise_error("Unsupported kernel_dims, must be 2D or 3D.", [f"Got kernel_dims={kernel_dims}"])
 
     op_utils.check_conv_pooling_args(kernel_dims, stride, padding)
+    if input.dtype == datatype.int8 and count_include_pad is False:
+        raise_error("`count_include_pad` can only be `True` when input datatype is int8.")
+
     stride = utils.utils.default(stride, [1] * spatial_dims)
     padding = utils.utils.default(padding, [(0, 0)] * spatial_dims)
+    pre_padding = [pad[0] for pad in padding]
+    post_padding = [pad[1] for pad in padding]
 
-    return op_utils.create_op(Pooling, [input], Pooling.Kind.AVG, kernel_dims, stride, padding)
+    return op_utils.create_op(
+        AvgPooling,
+        [input],
+        kernel_dims,
+        stride,
+        pre_padding,
+        post_padding,
+        avg_excludes_padding=not count_include_pad,
+    )
