ddde0f9d97373e87f69f8bb2011a4137f0394c25
pranavm <pranavm@nvidia.com>
2025-02-27 09:17:00 -0800

    Removes `copy` operation
    The copy operation lets us copy tensors between multiple GPUs or between
    the GPU and CPU. However, since TensorRT doesn't support either of these cases,
    the resulting executable would be more than just a single TRT engine.
    
    This change removes the copy operation for now.

--- tripy/tests/integration/test_repeat.py
+++ tripy/tests/integration/test_repeat.py
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: Copyright (c) 2024 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -16,7 +16,7 @@
 import nvtripy as tp
 
 import pytest
-import numpy as np
+import cupy as cp
 
 
 class TestRepeat:
@@ -31,17 +31,17 @@ class TestRepeat:
         ],
     )
     def test_repeat(self, repeats, dim, eager_or_compiled):
-        inp = np.arange(4, dtype=np.int32).reshape((2, 2))
+        inp = cp.arange(4, dtype=cp.int32).reshape((2, 2))
 
         out = eager_or_compiled(tp.repeat, tp.Tensor(inp), repeats, dim)
-        expected = np.repeat(inp, repeats, dim)
+        expected = cp.repeat(inp, repeats, dim)
 
-        assert np.array_equal(np.from_dlpack(tp.copy(out, device=tp.device("cpu"))), expected)
+        assert cp.array_equal(cp.from_dlpack(out), expected)
 
     def test_repeat_shape_scalar(self, eager_or_compiled):
-        inp = np.arange(4, dtype=np.int32).reshape((2, 2))
+        inp = cp.arange(4, dtype=cp.int32).reshape((2, 2))
         s = tp.ones((1, 2))
         out = eager_or_compiled(tp.repeat, tp.Tensor(inp), repeats=s.shape[1], dim=0)
-        expected = np.repeat(inp, 2, 0)
+        expected = cp.repeat(inp, 2, 0)
 
-        assert np.array_equal(np.from_dlpack(tp.copy(out, device=tp.device("cpu"))), expected)
+        assert cp.array_equal(cp.from_dlpack(out), expected)
