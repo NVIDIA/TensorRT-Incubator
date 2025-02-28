b4dc76af3c8547998f141c93ee9693454767b96b
pranavm <pranavm@nvidia.com>
2025-02-28 14:49:40 -0800

    First round of bug fixing

--- tripy/tests/logging/test_logging.py
+++ tripy/tests/logging/test_logging.py
@@ -1,5 +1,5 @@
 #
-# SPDX-FileCopyrightText: Copyright (c) 2024 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -39,11 +39,6 @@ class TestLogging:
             (
                 "info",
                 {"": {"info", "warning", "error"}},
-            ),
-            # Enable `flat_ir` logging in all modules
-            (
-                "flat_ir",
-                {"": {"flat_ir"}},
             ),
             # Enable `info` and `trace` logging in all modules
             (
