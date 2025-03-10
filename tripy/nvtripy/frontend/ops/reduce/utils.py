e8413c0305d8682b0c4aaa92ace03c7befd498b9
pranavm <pranavm@nvidia.com>
2025-03-10 11:16:41 -0700

    Fixes argmin/argmax for dim=None,keepdim=True, makes reduce work with scalars

--- tripy/nvtripy/frontend/ops/reduce/utils.py
+++ tripy/nvtripy/frontend/ops/reduce/utils.py
@@ -16,27 +16,52 @@
 from nvtripy.frontend.ops import utils as op_utils
 
 
+def reduce_impl(ReduceOpType, input, dim, keepdim):
+    if input.rank == 0:
+        return input
+
+    # TODO (pranavm): Test all reduction functions with scalars.
+    dim = op_utils.process_dim_sequence(dim, input.rank)
+    return op_utils.create_op(ReduceOpType, [input], dim, keepdim)
+
+
 def arg_min_max_impl(TopKType, input, dim, keepdim):
     from nvtripy.frontend.ops.reshape import reshape
     from nvtripy.frontend.ops.squeeze import squeeze
+    from nvtripy.frontend.ops.unsqueeze import unsqueeze
+    from nvtripy.frontend.tensor import Tensor
 
-    # TODO (pranavm): Top-K requires 2D inputs probably, so need to unsqueeze.
-    # Flatten should really just flatten instead.
     original_rank = input.rank
+
+    if original_rank == 0:
+        return Tensor(0, dtype=input.dtype)
+
+    # The semantics of argmin/argmax are that the input is treated as a
+    # flattened tensor if dim is not set, except the output rank should match
+    # the input rank if keepdim=True.
     should_flatten = dim is None
     if should_flatten:
-        input = reshape(input, (1, -1))
-        dim = 1
+        input = reshape(input, (-1,))
+        dim = 0
 
     dim = op_utils.process_dim(dim, input.rank)
 
+    # Top-K requires 2D inputs, so we must unsqueeze
+    should_unsqueeze_1D = input.rank == 1
+    if should_unsqueeze_1D:
+        input = unsqueeze(input, -1)
+
     _, indices = op_utils.create_op(TopKType, [input], dim=dim, k=1)
+
+    if should_unsqueeze_1D:
+        indices = squeeze(indices, -1)
 
     # Top-k always keeps dimensions
     if not keepdim:
         indices = squeeze(indices, dim)
 
-    if should_flatten:
+    # Only unflatten at the end if we need to keep the dimensions.
+    if should_flatten and keepdim:
         indices = reshape(indices, (1,) * original_rank)
 
     return indices
