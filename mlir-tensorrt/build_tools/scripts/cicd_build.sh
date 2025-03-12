3f7fe85ff87b8fd5be5ee0b02853bbf2b1ece146
pranavm <pranavm@nvidia.com>
2025-03-12 11:43:24 -0700

    Updates default TensorRT version to 10.9
    Updates the default version we compile against to 10.9.
    Also corrects the build number to match the latest release.

--- mlir-tensorrt/build_tools/scripts/cicd_build.sh
+++ mlir-tensorrt/build_tools/scripts/cicd_build.sh
@@ -5,7 +5,7 @@ set -o pipefail
 REPO_ROOT=$(pwd)
 BUILD_DIR="${REPO_ROOT}/build"
 export LLVM_LIT_ARGS=${LLVM_LIT_ARGS:-"-v --xunit-xml-output ${BUILD_DIR}/test-results.xml --timeout=1200 --time-tests -Drun_long_tests=${RUN_LONG_TESTS}"}
-export DOWNLOAD_TENSORRT_VERSION=${DOWNLOAD_TENSORRT_VERSION:-10.7}
+export DOWNLOAD_TENSORRT_VERSION=${DOWNLOAD_TENSORRT_VERSION:-10.9}
 export ENABLE_ASAN=${ENABLE_ASAN:-OFF}
 export CPM_SOURCE_CACHE=${CPM_SOURCE_CACHE:-/.cache.cpm}
 export CCACHE_DIR=${CCACHE_DIR:-/ccache}
