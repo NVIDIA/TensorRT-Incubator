56de88d8aaebbf79d5738502d12aa059d43469b1
pranavm <pranavm@nvidia.com>
2025-03-14 10:34:33 -0700

    Renames BaseTraceOp to TraceOp

--- tripy/nvtripy/trace/ops/gather.py
+++ tripy/nvtripy/trace/ops/gather.py
@@ -18,11 +18,11 @@
 from dataclasses import dataclass
 
 from mlir_tensorrt.compiler.dialects import tensorrt
-from nvtripy.trace.ops.base import BaseTraceOp
+from nvtripy.trace.ops.base import TraceOp
 
 
 @dataclass(repr=False)
-class Gather(BaseTraceOp):
+class Gather(TraceOp):
     dim: int
 
     def infer_rank(self):
