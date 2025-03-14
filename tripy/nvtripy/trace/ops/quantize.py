2fbbb787c89d049cef47d9f2cbc1e2e5efd88fc5
pranavm <pranavm@nvidia.com>
2025-03-14 10:34:33 -0700

    Renames BaseTraceOp to TraceOp

--- tripy/nvtripy/trace/ops/quantize.py
+++ tripy/nvtripy/trace/ops/quantize.py
@@ -1,5 +1,5 @@
 #
-# SPDX-FileCopyrightText: Copyright (c) 2024 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -20,11 +20,11 @@ from dataclasses import dataclass
 from mlir_tensorrt.compiler.dialects import tensorrt
 from nvtripy.common import datatype
 from nvtripy.trace.ops import utils as op_utils
-from nvtripy.trace.ops.base import BaseTraceOp
+from nvtripy.trace.ops.base import TraceOp
 
 
 @dataclass(repr=False)
-class Quantize(BaseTraceOp):
+class Quantize(TraceOp):
 
     dtype: datatype.dtype
     dim: int
