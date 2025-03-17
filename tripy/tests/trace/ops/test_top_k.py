3edd0095b0bedc6024a3789f01b3dde579b7d01e
pranavm <pranavm@nvidia.com>
2025-03-17 14:16:54 -0700

    Correct topk -> top_k

---
+++ tripy/tests/trace/ops/test_top_k.py
@@ -0,0 +1,37 @@
+# SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import nvtripy as tp
+
+from nvtripy.trace.ops.top_k import TopK
+
+
+class TestTopK:
+    def test_infer_rank(self):
+        inp = tp.ones((2, 2, 3))
+        values, indices = TopK([inp.trace_tensor], dim=2, k=2).outputs
+        assert values.rank == inp.rank
+        assert indices.rank == inp.rank
+
+    def test_infer_dtypes(self):
+        inp = tp.ones((2, 2, 3))
+        values, indices = TopK([inp.trace_tensor], dim=2, k=2).outputs
+        assert values.dtype == inp.dtype
+        assert indices.dtype == tp.int32
+
+    def test_infer_devices(self):
+        inp = tp.ones((2, 2, 3))
+        values, indices = TopK([inp.trace_tensor], dim=2, k=2).outputs
+        assert values.device == inp.device
+        assert indices.device == inp.device
