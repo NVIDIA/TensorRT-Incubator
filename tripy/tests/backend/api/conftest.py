7e426d715eb4f19c254a564a3ae480391baf1d0c
pranavm <pranavm@nvidia.com>
2025-03-18 16:38:32 -0700

    Adds support for variadic arguments in `tp.compile`

--- tripy/tests/backend/api/conftest.py
+++ tripy/tests/backend/api/conftest.py
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: Copyright (c) 2024 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -33,8 +33,8 @@ def returns_multiple_tensors(a, b):
 
 
 def variadic_positional(*args):
-    pass
+    return sum(args)
 
 
 def variadic_keyword(**kwargs):
-    pass
+    return sum(kwargs.values())
