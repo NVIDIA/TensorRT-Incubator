b8e0b86d3b7319e2fba43d8da871308e13238f69
pranavm <pranavm@nvidia.com>
2025-03-25 13:46:37 -0700

    Rewrites pooling tests

--- tripy/tests/integration/test_pooling.py
+++ tripy/tests/integration/test_pooling.py
@@ -19,72 +19,42 @@ import pytest
 import torch
 
 import nvtripy as tp
+import math
 
 
 class TestPooling:
-
     @pytest.mark.parametrize(
         "kernel_dims, stride, padding",
         [
             ((3, 3), (1, 1), ((0, 0), (0, 0))),
             ((4, 4), (2, 2), ((1, 1), (2, 2))),
-        ],
-    )
-    @pytest.mark.parametrize("dtype", [tp.float32, tp.float16, tp.int8])
-    @pytest.mark.parametrize("pool_type", ["max", "avg"])
-    def test_pool_2d(self, kernel_dims, stride, padding, dtype, pool_type, eager_or_compiled):
-        inp_tp = tp.reshape(tp.arange(64, dtype=dtype), (1, 1, 8, 8))
-        torch_padding = (padding[0][0], padding[1][0])
-
-        if pool_type == "avg" and dtype == tp.int8:
-            # TODO (pranavm): Check this:
-            pytest.skip("Torch average pool is not implemented for int8")
-
-        if pool_type == "max":
-            out = eager_or_compiled(tp.maxpool, inp_tp, kernel_dims=kernel_dims, stride=stride, padding=padding)
-            pool_torch = torch.nn.MaxPool2d(kernel_size=kernel_dims, stride=stride, padding=torch_padding)
-        elif pool_type == "avg":
-            if torch_padding != (0, 0):
-                # TODO (pranavm): Check this - probably due to including/not including padding.:
-                pytest.skip(
-                    "https://github.com/NVIDIA/TensorRT-Incubator/issues/241: Tripy average pool is incorrect when padding != 0."
-                )
-
-            out = eager_or_compiled(tp.avgpool, inp_tp, kernel_dims=kernel_dims, stride=stride, padding=padding)
-            pool_torch = torch.nn.AvgPool2d(kernel_size=kernel_dims, stride=stride, padding=torch_padding)
-
-        out_torch = torch.from_dlpack(out).to("cpu")
-        expected = pool_torch(torch.from_dlpack(inp_tp).to("cpu"))
-        assert torch.allclose(expected, out_torch)
-        assert expected.shape == out_torch.shape
-
-    @pytest.mark.parametrize(
-        "kernel_dims, stride, padding",
-        [
             ((2, 2, 2), (2, 2, 2), ((0, 0), (1, 1), (1, 1))),
         ],
     )
     @pytest.mark.parametrize("dtype", [tp.float32, tp.float16])
     @pytest.mark.parametrize("pool_type", ["max", "avg"])
-    def test_pool_3d(self, kernel_dims, stride, padding, dtype, pool_type, eager_or_compiled):
-        inp_tp = tp.reshape(tp.arange(512, dtype=dtype), (1, 1, 8, 8, 8))
-        torch_padding = (padding[0][0], padding[1][0], padding[2][0])
-
-        if torch_padding != (0, 0, 0):
-            # TODO (pranavm): Check this - same as above:
-            pytest.skip(
-                "https://github.com/NVIDIA/TensorRT-Incubator/issues/241: Tripy average pool is incorrect when padding != 0."
-            )
+    def test_pool(self, kernel_dims, stride, padding, dtype, pool_type, eager_or_compiled):
+        input_shape = (1, 1) + (8,) * len(kernel_dims)
+        inp_tp = tp.reshape(tp.arange(math.prod(input_shape), dtype=dtype), input_shape)
+        torch_padding = []
+        # Torch padding starts from the last dimension
+        for tup in reversed(padding):
+            torch_padding.extend(tup)
 
         if pool_type == "max":
             out = eager_or_compiled(tp.maxpool, inp_tp, kernel_dims=kernel_dims, stride=stride, padding=padding)
-            pool_torch = torch.nn.MaxPool3d(kernel_size=kernel_dims, stride=stride, padding=torch_padding)
+
+            TorchPoolType = torch.nn.MaxPool2d if len(kernel_dims) == 2 else torch.nn.MaxPool3d
+            pool_torch = TorchPoolType(kernel_size=kernel_dims, stride=stride)
         elif pool_type == "avg":
             out = eager_or_compiled(tp.avgpool, inp_tp, kernel_dims=kernel_dims, stride=stride, padding=padding)
-            pool_torch = torch.nn.AvgPool3d(kernel_size=kernel_dims, stride=stride, padding=torch_padding)
 
-        out_torch = torch.from_dlpack(out).to("cpu")
+            TorchPoolType = torch.nn.AvgPool2d if len(kernel_dims) == 2 else torch.nn.AvgPool3d
+            pool_torch = TorchPoolType(kernel_size=kernel_dims, stride=stride)
 
-        expected = pool_torch(torch.from_dlpack(inp_tp).to("cpu"))
+        out_torch = torch.from_dlpack(out)
+
+        padded_inp = torch.nn.functional.pad(torch.from_dlpack(inp_tp), torch_padding)
+        expected = pool_torch(padded_inp)
         assert torch.allclose(expected, out_torch)
         assert expected.shape == out_torch.shape
