881ebea52c420e431123dc8d915d6a88f544c32f
Sagar Shelke <sagshelke@nvidia.com>
2025-03-27 14:04:02 -0700

    [mlir-tensorrt] Wheel release script fix (#592)
    This PR fixes an issue in wheel release bash script.

--- mlir-tensorrt/build_tools/scripts/build_wheels.sh
+++ mlir-tensorrt/build_tools/scripts/build_wheels.sh
@@ -1,23 +1,19 @@
 #!/usr/bin/env bash
 # Builds Python wheels for MLIR-TensorRT packages using the specified python version number.
-# Usage:
-# build_wheels.sh [python version] [TensorRT version]
-# e.g. build_wheels.sh 3.X 10.X
+# e.g.
+# PY_VERSION=3.10 build_wheels.sh
+# PY_VERSION=3.10 DOWNLOAD_TENSORRT_VERSION=10.7 build_wheels.sh
 set -e
-py_version=${PY_VERSION:-${1:-3.10}}
+py_version=${PY_VERSION:-3.10}
 
 PATH=$PATH:/pyenv/bin
 mkdir -p .private.wheels || true
 pyenv local ${py_version}
 python${py_version} -m pip install -r python/requirements-dev.txt
 
-# If `DOWNLOAD_TENSORRT_VERSION` is not set, use the second argument as the version number.
-if [ -z "${DOWNLOAD_TENSORRT_VERSION}" ]; then
-    DOWNLOAD_TENSORRT_VERSION=$2
-    export DOWNLOAD_TENSORRT_VERSION
-fi
+export DOWNLOAD_TENSORRT_VERSION=${DOWNLOAD_TENSORRT_VERSION:-10.7}
 
 rm -rf build || true
-cmake --preset ninja-clang-wheel-release
+cmake --preset ninja-release-wheels
 ninja -C build mlir-tensorrt-all-wheels
 rsync -za build/wheels/ .private.wheels/
