277717bf3946c01427eda776848002be12546cca
pranavm <pranavm@nvidia.com>
2025-04-08 13:05:44 -0700

    Bumps version to 0.1.0, updates MLIR-TRT dependency versions

--- tripy/nvtripy/__init__.py
+++ tripy/nvtripy/__init__.py
@@ -1,5 +1,5 @@
 #
-# SPDX-FileCopyrightText: Copyright (c) 2024 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -15,7 +15,7 @@
 # limitations under the License.
 #
 
-__version__ = "0.0.10"
+__version__ = "0.1.0"
 
 # Import TensorRT to make sure all dependent libraries are loaded first.
 import tensorrt
