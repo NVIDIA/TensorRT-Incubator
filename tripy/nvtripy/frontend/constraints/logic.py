37627fc178f931257b2ef0625d0f3b26bb94362f
pranavm <pranavm@nvidia.com>
2025-10-28 11:23:29 -0700

    Improves constraint error messages

--- tripy/nvtripy/frontend/constraints/logic.py
+++ tripy/nvtripy/frontend/constraints/logic.py
@@ -15,14 +15,11 @@
 # limitations under the License.
 #
 from abc import abstractmethod
-from typing import Any, List, Sequence, Tuple
+from typing import Any, List, Optional, Sequence, Tuple
 
 from nvtripy.frontend.constraints.base import Constraints
 from nvtripy.frontend.constraints.fetcher import Fetcher
 from nvtripy.utils.result import Result
-
-# TODO (pranavm): Maybe error details should say what went wrong, not what was expected?
-# TODO (pranavm): Maybe return a list of violated constraints? Then we can construct error messages outside.
 
 
 class Logic(Constraints):
@@ -31,7 +28,7 @@ class Logic(Constraints):
     """
 
     @abstractmethod
-    def __call__(self, args: List[Tuple[str, Any]]) -> Result: ...
+    def __call__(self, args: List[Tuple[str, Any]], returns: Optional[Tuple[Any]] = None) -> Result: ...
 
     def __and__(self, other: "Logic") -> "Logic":
         if isinstance(self, And):
@@ -59,20 +56,22 @@ class OneOf(Logic):
         # Need to convert generator expressions so we can use them more than once
         self.options = list(options)
 
-    def __call__(self, args: List[Tuple[str, Any]]) -> Result:
-        value = self.fetcher(args)
+    def __call__(self, args: List[Tuple[str, Any]], returns: Optional[Tuple[Any]] = None) -> Result:
+        value = self.fetcher(args, returns)
         if value in self.options:
             return Result.ok()
 
-        return Result.err([f"'{self.fetcher}' to be one of {self.options}, but got {value}"])
+        return Result.err([f"'{self.fetcher}' to be one of {self.options} (but it was '{value}')"])
 
     def __str__(self):
         return f"{self.fetcher} is one of {self.options}"
 
 
-def get_val_or_call_fetcher(fetcher_or_value: Any, args: List[Tuple[str, Any]]) -> Any:
+def get_val_or_call_fetcher(
+    fetcher_or_value: Any, args: List[Tuple[str, Any]], returns: Optional[Tuple[Any]] = None
+) -> Any:
     if isinstance(fetcher_or_value, Fetcher):
-        return fetcher_or_value(args)
+        return fetcher_or_value(args, returns)
     return fetcher_or_value
 
 
@@ -81,13 +80,13 @@ class Equal(Logic):
         self.fetcher = fetcher
         self.fetcher_or_value = fetcher_or_value
 
-    def __call__(self, args: List[Tuple[str, Any]]) -> Result:
-        value1 = self.fetcher(args)
-        value2 = get_val_or_call_fetcher(self.fetcher_or_value, args)
+    def __call__(self, args: List[Tuple[str, Any]], returns: Optional[Tuple[Any]] = None) -> Result:
+        value1 = self.fetcher(args, returns)
+        value2 = get_val_or_call_fetcher(self.fetcher_or_value, args, returns)
         if value1 == value2:
             return Result.ok()
 
-        return Result.err([f"'{self.fetcher}' to be equal to '{self.fetcher_or_value}'"])
+        return Result.err([f"'{self.fetcher}' to be equal to '{self.fetcher_or_value}' (but it was '{value1}')"])
 
     def __str__(self):
         return f"{self.fetcher} == {self.fetcher_or_value}"
@@ -98,27 +97,26 @@ class NotEqual(Logic):
         self.fetcher = fetcher
         self.fetcher_or_value = fetcher_or_value
 
-    def __call__(self, args: List[Tuple[str, Any]]) -> Result:
-        value1 = self.fetcher(args)
-        value2 = get_val_or_call_fetcher(self.fetcher_or_value, args)
+    def __call__(self, args: List[Tuple[str, Any]], returns: Optional[Tuple[Any]] = None) -> Result:
+        value1 = self.fetcher(args, returns)
+        value2 = get_val_or_call_fetcher(self.fetcher_or_value, args, returns)
         if value1 != value2:
             return Result.ok()
 
-        return Result.err([f"'{self.fetcher}' to be not equal to '{self.fetcher_or_value}'"])
+        return Result.err([f"'{self.fetcher}' to be not equal to '{self.fetcher_or_value}' (but it was '{value1}')"])
 
     def __str__(self):
         return f"{self.fetcher} != {self.fetcher_or_value}"
 
 
-# TODO (pranavm): Make And and Or combine errors and include a prefix like: "{...} but condition XYZ failed."
 class And(Logic):
     def __init__(self, *constraints: Logic):
         self.constraints = constraints
 
-    def __call__(self, args: List[Tuple[str, Any]]) -> Result:
+    def __call__(self, args: List[Tuple[str, Any]], returns: Optional[Tuple[Any]] = None) -> Result:
         errors = []
         for constraint in self.constraints:
-            result = constraint(args)
+            result = constraint(args, returns)
             if not result:
                 errors.extend(([" and "] if errors else []) + result.error_details)
         if errors:
@@ -133,10 +131,10 @@ class Or(Logic):
     def __init__(self, *constraints: Logic):
         self.constraints = constraints
 
-    def __call__(self, args: List[Tuple[str, Any]]) -> Result:
+    def __call__(self, args: List[Tuple[str, Any]], returns: Optional[Tuple[Any]] = None) -> Result:
         all_errors = []
         for constraint in self.constraints:
-            result = constraint(args)
+            result = constraint(args, returns)
             if result:
                 return Result.ok()
             all_errors.extend(([" or "] if all_errors else []) + result.error_details)
@@ -150,8 +148,8 @@ class Not(Logic):
     def __init__(self, constraint: Logic):
         self.constraint = constraint
 
-    def __call__(self, args: List[Tuple[str, Any]]) -> Result:
-        result = self.constraint(args)
+    def __call__(self, args: List[Tuple[str, Any]], returns: Optional[Tuple[Any]] = None) -> Result:
+        result = self.constraint(args, returns)
         if result:
             return Result.err([str(self)])
         return Result.ok()
