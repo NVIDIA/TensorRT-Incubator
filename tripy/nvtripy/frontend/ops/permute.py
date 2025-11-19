a57e59f12387f1c4c6abab1828094d1bdd8604f5
pranavm <pranavm@nvidia.com>
2025-11-19 19:29:15 +0000

    Migrate shape manipulation operators to new constraint system

--- tripy/nvtripy/frontend/ops/permute.py
+++ tripy/nvtripy/frontend/ops/permute.py
@@ -18,18 +18,22 @@
 from typing import Sequence
 
 from nvtripy import export
+from nvtripy.common import datatype as dt
 from nvtripy.common.exception import raise_error
 from nvtripy.frontend.ops import utils as op_utils
 from nvtripy.frontend.ops._registry import register_tensor_method
 from nvtripy.trace.ops.permute import Permute
 from nvtripy.frontend import wrappers
+from nvtripy.frontend.constraints import GetInput, GetReturn, OneOf
 
 
 @register_tensor_method("permute")
 @export.public_api(document_under="operations/functions")
 @wrappers.interface(
-    dtype_constraints={"input": "T1", wrappers.RETURN_VALUE: "T1"},
-    dtype_variables={"T1": ["float32", "float16", "bfloat16", "int4", "int8", "int32", "int64", "bool"]},
+    input_requirements=OneOf(
+        GetInput("input").dtype, [dt.float32, dt.float16, dt.bfloat16, dt.int4, dt.int8, dt.int32, dt.int64, dt.bool]
+    ),
+    output_guarantees=GetReturn(0).dtype == GetInput("input").dtype,
 )
 def permute(input: "nvtripy.Tensor", perm: Sequence[int]) -> "nvtripy.Tensor":
     """
