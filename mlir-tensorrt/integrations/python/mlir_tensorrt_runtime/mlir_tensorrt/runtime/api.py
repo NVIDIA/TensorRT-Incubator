f76294a6daab42812fc43668987325a0740b0736
Christopher Bate <cbate@nvidia.com>
2026-02-18 20:02:41 +0000

    [python] Force initialization of tvm_ffi in the mlir_tensorrt_runtime package
    The runtime pybind extension links the tvm_ffi library dynamically; we
    need to ensure it is initialized before loading the extension module.
    Otherwise, importing the runtime will cause a runtime error since OS
    loader will not be able to find the `tvm_ffi.so` library.
    
    GitOrigin-RevId: 68c5dad17d202e971ca10d2a98e1099f7043e3ff

--- mlir-tensorrt/integrations/python/mlir_tensorrt_runtime/mlir_tensorrt/runtime/api.py
+++ mlir-tensorrt/integrations/python/mlir_tensorrt_runtime/mlir_tensorrt/runtime/api.py
@@ -1,1 +1,10 @@
+# Force initializtion of tvm_ffi before loading the extension module.
+try:
+    import tvm_ffi
+except Exception as e:
+    raise RuntimeError(
+        "Error importing tvm_ffi, which is required for mlir-tensorrt-runtime: %s",
+        e,
+    )
+
 from ._mlir_libs._api import *
