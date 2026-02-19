b81555b6f6320c4fabc85d4e99e6fb56427518e6
pranavm <pranavm@nvidia.com>
2026-02-19 18:35:50 +0000

    Updates version to 0.1.5

--- tripy/nvtripy/__init__.py
+++ tripy/nvtripy/__init__.py
@@ -1,5 +1,5 @@
 #
-# SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-FileCopyrightText: Copyright (c) 2026 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -15,7 +15,7 @@
 # limitations under the License.
 #
 
-__version__ = "0.1.4"
+__version__ = "0.1.5"
 
 # Import TensorRT to make sure all dependent libraries are loaded first.
 import tensorrt
