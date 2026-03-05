31b19dd55790547db741646967a7c30cc12d7a5f
pranavm <pranavm@nvidia.com>
2026-03-05 18:56:59 +0000

    Updates dependencies for examples and notebooks

--- tripy/tests/test_examples.py
+++ tripy/tests/test_examples.py
@@ -1,5 +1,5 @@
 #
-# SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-FileCopyrightText: Copyright (c) 2026 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -87,7 +87,7 @@ EXAMPLES = [
     Example(["nanogpt"]),
     Example(
         ["segment-anything-model-v2"],
-        artifact_names=["truck.jpg", "bedroom", "saved_engines_l/", "output/", "checkpoints/*.pt"],
+        artifact_names=["truck.jpg", "bedroom", "saved_engines_*/", "output/", "checkpoints/*.pt"],
     ),
     Example(["diffusion"], artifact_names=["output/", "fp16_engines/"]),
 ]
